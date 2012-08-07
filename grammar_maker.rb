f0bcbeeb3e2ab26ee18b932d9f09a882224a9c60
Ben Shapiro <ben@getdown.org>
2012-08-07 13:27:01 -0500

    now we can group strings

--- grammar_maker.rb
+++ grammar_maker.rb
@@ -1,12 +1,27 @@
+require 'poisson'
 class Array
   def random_element
     self[rand(self.length)]
   end
 end
 
-Symbols = [:a, :b, :c, :d, :e]
-Operators = ['*', '+', '.*']
+#borrowed from: http://www.ruby-forum.com/topic/104729
+def poisson_random lambda
+   l = Math.exp(-lambda)
+   k = 0
+   p = 1
+   while p >= l
+     k += 1
+     u = rand
+     p = p *u
+   end
+   k - 1
+end
 
+Symbols = [:a, :b, :c, :d, :e, '.']
+Operators = ['*', '+']
+
+#todo: add grouping (e.g., repeated strings)
 def make_random_grammar(symbols, operators, length)
   if length == 0
     []
@@ -14,13 +29,21 @@ def make_random_grammar(symbols, operators, length)
     Array(symbols.random_element)
   else
     preceding = make_random_grammar(symbols, operators, length - 1)
-    if operators.include? preceding.last #we don't want to chain two operators together
+    if operators.include? preceding.last #we don't want to chain two operators together, but may want to add a group
+      groupiness = [preceding.length + 1, poisson_random(3)].min
+      if groupiness <= 3 #no need to group
+      else
+        preceding.insert(0-groupiness, '(')
+        preceding.insert(-2, ')')
+      end
       preceding + Array(symbols.random_element)
     else
       preceding + Array((symbols + operators).random_element)
     end
   end    
 end
+
+
 
 10.times do |length|
   (Symbols.length ** length).times do 
