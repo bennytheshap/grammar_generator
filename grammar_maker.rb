e3a3d52d2f936a6524b40faae788598bc08e8b1c
Ben Shapiro <ben@getdown.org>
2012-08-07 16:59:21 -0500

    prettier output

--- grammar_maker.rb
+++ grammar_maker.rb
@@ -6,6 +6,6 @@ Log_Symbols = ['.', :apple, :bar, :charlie, :dead, :evil, :funny, :gasp]
   ((Log_Symbols.length-1) ** length).times do 
     grammar = make_random_grammar(Log_Symbols, length)
     singlified = singlify_string(Log_Symbols, grammar)
-    puts "#{singlified} <-- #{grammar.inspect}"
+    puts "#{singlified} \t\t\t<-- #{grammar.inspect}"
   end
 end
