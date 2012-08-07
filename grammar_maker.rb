4c88ada8405b0e9bbd52bae038f34031dfffdb10
Ben Shapiro <ben@getdown.org>
2012-08-07 16:05:06 -0500

    fewer samples per length

--- grammar_maker.rb
+++ grammar_maker.rb
@@ -3,7 +3,7 @@ require_relative 'utils.rb'
 Log_Symbols = ['.', :foo, :bar, :charlie, :dead, :evil]
 
 10.times do |length|
-  (Log_Symbols.length ** length).times do 
+  ((Log_Symbols.length-1) ** length).times do 
     grammar = make_random_grammar(Log_Symbols, length)
     singlified = singlify_string(Log_Symbols, grammar)
     puts "#{singlified} <-- #{grammar.inspect}"
