c49b9185172099119384b8ce293a676786fd469a
Ben Shapiro <ben@getdown.org>
2012-08-07 14:34:29 -0500

    cleanup and debugging output

--- grammar_maker.rb
+++ grammar_maker.rb
@@ -4,7 +4,8 @@ Log_Symbols = ['.', :foo, :bar, :charlie, :dead, :evil]
 
 10.times do |length|
   (Log_Symbols.length ** length).times do 
-    output = singlify_string(Log_Symbols, make_random_grammar(Log_Symbols, length))
-    puts output.inspect
+    grammar = make_random_grammar(Log_Symbols, length)
+    singlified = singlify_string(Log_Symbols, grammar)
+    puts "#{singlified} <-- #{grammar.inspect}"
   end
 end
