9649fb1d1e4c90396ef67f4074cf86e500d64216
Ben Shapiro <ben@getdown.org>
2012-08-07 22:28:38 -0500

    changed to use the progX symbols and to make longer strings

--- grammar_maker.rb
+++ grammar_maker.rb
@@ -1,11 +1,14 @@
 require_relative 'utils.rb'
 
-Log_Symbols = ['.', :apple, :bar, :charlie, :dead, :evil, :funny, :gasp]
-
-10.times do |length|
-  ((Log_Symbols.length-1) ** length).times do 
+#Log_Symbols = ['.', :apple, :bar, :charlie, :dead, :evil, :funny, :gasp]
+Log_Symbols = ['.', :MissionActionData, :PopulateGridData, :GridStateData, :DialogButtonData, :InventorySelectedData, :ToolSelectedData, :ToolUseData, :CellCollectionData, :ObjectiveActionData, :ViewSelectedData, :TissueCollectionData, :GridDestroyData, :AlmanacView]
+20.times do |length|
+  #((Log_Symbols.length-1) ** length).times do 
+  100.times do 
     grammar = make_random_grammar(Log_Symbols, length)
     singlified = singlify_string(Log_Symbols, grammar)
+    next if singlified.strip.length == 0
     puts "#{singlified} \t\t\t<-- #{grammar.inspect}"
+    #puts singlified
   end
 end
