235f07de0631bb3928f66b1546104105b2954dd4
Ben Shapiro <ben@getdown.org>
2012-08-17 11:28:29 -0500

    tweak the parameters for grammar generation

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 
 gem 'poisson'
+gem 'bloomfilter-rb'
--- grammar_maker.rb
+++ grammar_maker.rb
@@ -1,17 +1,29 @@
 require_relative 'utils.rb'
+require 'set'
+require 'bloomfilter-rb'
 
 #Log_Symbols = ['.', :apple, :bar, :charlie, :dead, :evil, :funny, :gasp]
 Log_Symbols = ['.', :MissionActionData, :PopulateGridData, :GridStateData, :DialogButtonData, :InventorySelectedData, :ToolSelectedData, :ToolUseData, :CellCollectionData, :ObjectiveActionData, :ViewSelectedData, :TissueCollectionData, :GridDestroyData, :AlmanacView]
 Stop_Symbols = [:GridStateData]
 
-20.times do |length|
+already_made = BloomFilter::Native.new(:size => 2**24, :bucket => 8, :raise => true)
+count = 1
+10.times do |length|
   how_many_times = (Log_Symbols.length - 1) ** (length + 1)
+  how_many_times = 100000 #just a few for testing purposes
   how_many_times.times do 
-  #100.times do 
     grammar = make_random_grammar(Log_Symbols - Stop_Symbols, length)
-    singlified = singlify_string(Log_Symbols, grammar)
-    next if singlified.strip.length == 0
-    #puts "#{singlified} \t\t\t<-- #{grammar.inspect}"
-    puts singlified
+    singlified = singlify_string(Log_Symbols, grammar).strip
+    if (singlified.length == 0) or (already_made.include? singlified)
+      next
+    else
+      already_made.insert singlified
+      #puts "#{count}: #{singlified}"
+      puts singlified
+    end
+    count += 1
   end
 end
+
+
+#$stderr.puts already_made.stats
